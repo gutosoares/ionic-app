ddcec37c8a5d21781e616743bc0da2314d7f7b60
Augusto Soares <gutosoares@outlook.com>
2017-05-09 20:46:10 -0300

    Funcao para adicionar tarefas esta finalizada

--- www/js/app.js
+++ www/js/app.js
@@ -12,14 +12,28 @@ app.run(function($ionicPlatform) {
   });
 })
 
-app.controller('mainController', function($scope) {
+app.controller('mainController', function($scope, $ionicModal) {
   var tasks = new getTasks();
 
   $scope.list = tasks.items;
+  $scope.task = {description: '', done: false};
   $scope.removeStatus = false;
+
+  $scope.showModal = function() {
+    $scope.modal.show();
+  };
 
   $scope.getShitDone = function(item) {
     item.done = !item.done;
+  };
+
+  $scope.onAddItem = function(item) {
+    // var item = {description: 'teste', done: false}
+
+    // showModal();
+    tasks.add(item);
+
+    $scope.modal.hide();
   };
 
   $scope.onRemove = function(item) {
@@ -28,5 +42,12 @@ app.controller('mainController', function($scope) {
 
   $scope.onClickRemove = function() {
     $scope.removeStatus = !$scope.removeStatus;
-  }
+  };
+
+  $ionicModal.fromTemplateUrl('add-item.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+  });
 })
